7e7f87927c7231b77efcee14b8d413eafc6daa0a
Sathaporn Sunthornpan <sathaphorn.stp@gmail.com>
2020-11-03 00:13:34 +0700

    feat: share and reuse functionality with libraries

--- libs/store/feature-game-detail/src/lib/store-feature-game-detail.tsx
+++ libs/store/feature-game-detail/src/lib/store-feature-game-detail.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { RouteComponentProps } from 'react-router-dom';
 
 import { makeStyles } from '@material-ui/core/styles';
@@ -7,6 +7,9 @@ import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
+import CardMedia from '@material-ui/core/CardMedia';
+
+import { formatRating } from '@example-workspace/store/util-formatters';
 
 import './store-feature-game-detail.scss';
 
@@ -24,20 +27,70 @@ export interface StoreFeatureGameDetailProps
 
 export const StoreFeatureGameDetail = (props: StoreFeatureGameDetailProps) => {
   const classes = useStyles();
+  const [state, setState] = useState<{
+    data: any;
+    loadingState: 'success' | 'error' | 'loading';
+  }>({
+    data: {},
+    loadingState: 'success',
+  });
 
-  return (
-    <Container className={classes.root}>
+  useEffect(() => {
+    setState({
+      ...state,
+      loadingState: 'loading',
+    });
+
+    const gameId = props.match.params.id;
+    fetch(`api/games/${gameId}`)
+      .then((x) => x.json())
+      .then((res) => {
+        setState({
+          ...state,
+          data: res,
+          loadingState: 'success',
+        });
+      })
+      .catch((err) => {
+        setState({
+          ...state,
+          loadingState: 'error',
+        });
+      });
+  }, [props.match.params.id]);
+
+  const renderContent = () => {
+    const { loadingState, data } = state;
+    if (loadingState === 'loading') {
+      return 'Loading...';
+    }
+
+    if (loadingState === 'error') {
+      return '<div>Error retrieving data</div>';
+    }
+
+    if (data == null) {
+      return '';
+    }
+
+    return (
       <Card>
         <CardActionArea>
+          <CardMedia image={state.data.image} title={state.data.name} />
           <CardContent>
             <Typography variant="body2" color="textSecondary" component="p">
-              {props.match.params.id}
+              {state.data.name}
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+              <strong>Rating:</strong> {formatRating(state.data.rating)}
             </Typography>
           </CardContent>
         </CardActionArea>
       </Card>
-    </Container>
-  );
+    );
+  };
+
+  return <Container className={classes.root}>{renderContent()}</Container>;
 };
 
 export default StoreFeatureGameDetail;
